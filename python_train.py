1f806c99a1f4a899cc845dfaa526fc3e65d1a31e
ahamee <hameed2771994@gmail.com>
2020-04-25 09:04:37 +0530

    Tesy

---
+++ python_train.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+#today = datetime.now()
+#print('todays date is: '+ str(today))
+
+## timedelta is used to deine a period of time
+#one_day = timedelta(days=2)
+#yesterday = (today - one_day)
+#print('Date before two days was: ' + str(yesterday))
+
+## getting only exact days and months and year
+#print('todays date is: '+ str(today.day))
+#print('todays date is: '+ str(today.month))
+#print('todays date is: '+ str(today.year))
+
+# when date is taken a input
+#birthday = input('when is your birthday (dd/mm/yyyy) ?')
+#DOB = datetime.strptime(birthday, '%m/%d/%Y')
+#print('Birthday ' + str(DOB))
+
+# Error Handeling

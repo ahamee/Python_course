c811110b71b9bcd434cccd0e289aca4b997ba554
ahamee <hameed2771994@gmail.com>
2020-04-30 08:30:47 +0530

    dictionaries

--- python_train.py
+++ python_train.py
@@ -18,3 +18,105 @@ from datetime import datetime, timedelta
 #print('Birthday ' + str(DOB))
 
 # Error Handeling
+x = 42
+y = 0
+#try:
+#    print(x/y)
+#except ZeroDivisionError as e:
+#    print('Not allowed to divide by zero')
+#else:
+#    print('Somehing else is wrong')
+#finally:
+#    print('This happens sometimes')
+
+## Conditions
+#if x == y:
+#    print('y')
+#elif x > y:
+#    print('G')
+#else:
+#    print("N")
+
+# List
+names = ['chudi ba', 'abdul', 'jaffer', 'waseem']
+
+# append will add object to the end of the list
+names.append('nadeem')
+#names.append(99)
+
+# insert will add object at a particular index position
+names.insert(0, 'javeed')
+
+# Replace
+names[1] = 'aaquib'
+
+# pop method remves the object based on the vaue passed into the pop method
+#poped_name = names.pop()
+
+# deleting elements from a list
+#del names[0]
+
+# removing and element from list
+#names.remove('aaquib')
+
+# Sort List in order basically it will consider in asc
+#names.sort(reverse=True)
+
+# sortig List temporary
+#sorted(names)
+
+# reversing a list
+#names.reverse()
+
+# length of List
+#print(len(names))
+
+#Looping through List
+#count = len(names)
+#for i in range (0,count):
+#    print(names[i])
+# OR
+#for NAME in names:
+#    print(NAME)
+
+#getting a numerical list
+#ODD = list(range(10,1,-1))
+#print(ODD)
+
+## generating square numbers and adding it to a list
+#squaroot = []
+#for i in range(1,10):
+#    sqr = i**2
+#    squaroot.append(sqr)
+#print(squaroot)
+#squares = [value**2 for value in range(1,11)]
+#print(squares)
+
+
+## slicing a list
+#print(names[3:-2])
+## ooping through sliing list
+#for name in names[2:6]:
+#    print(name)
+
+## Tuple Values cannot be changed once assigne hence they are called immutable
+#dimensions = (100,20)
+#print(dimensions[0])
+
+#num = int(input("enter number: \t"))
+#print(type(num))
+
+## Dictionaries
+#car = {}
+#car['Roma'] = 'ferrari'
+#car['Civic'] = 'Honda'
+#car['Hell cat'] = 'Dodge'
+#print(car)
+
+car = {
+    'jenny' : 'female',
+     'lang' : ['C','C#','Java'],
+    'mac' : 'male',
+    'langu' : ['go lang', 'python']
+}
+print(car['jenny'])
